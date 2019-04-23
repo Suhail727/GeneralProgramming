bb0c4a2febdc1437b32cedc14af43c4c00ed1a81
Mohammed Suhail <Suhail727@users.noreply.github.com>
2019-04-24 01:11:36 +0530

    Update Sorting (Merge Sort).py

--- Sorting (Merge Sort).py
+++ Sorting (Merge Sort).py
@@ -1,13 +1,20 @@
 def mergeSort(a):
+  # Sort only if there is more thanone element in array
   if len(a)>1:
+    # Find the middle index and create 2 lists, left and right.
     mid = len(a)//2
     l = a[:mid]
     r = a[mid:]
+    # Call the function recursively until left and right lists have only one element each
     mergeSort(l)
     mergeSort(r)
-
+    # Merge Function
+    # i for left list, j for right list, k for main list
     i = j = k = 0
+    # i and j should be within size of left and right list. If they aren't, it means either list has become empty.
+    # In that case move to next while loop
     while i < len(l) and j < len(r):
+      # Check which is smaller value from left and right list, whichever is smaller append to main list
       if l[i] < r[j]:
         a[k] = l[i]
         i += 1
@@ -15,6 +22,7 @@ def mergeSort(a):
         a[k] = r[j]
         j += 1
       k += 1
+    # If either left or right list becomes empty, just append the remaining list elements to the main list.
     while i < len(l):
       a[k] = l[i]
       i += 1
