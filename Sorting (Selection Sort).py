97725c658f956791b842cb913052ebd51c5933d9
Mohammed Suhail <Suhail727@users.noreply.github.com>
2019-04-23 13:10:54 +0530

    Update Sorting (Selection Sort).py

--- Sorting (Selection Sort).py
+++ Sorting (Selection Sort).py
@@ -1,15 +1,16 @@
 A = [64, 25, 12, 22, 11] 
   
-# Traverse through all array elements 
+# Traverse through all array elements, find min value and swap with first value
 for i in range(len(A)): 
+    # Consider first index element as minimum
+    min_idx = i 
     # Find the minimum element in remaining  
     # unsorted array 
-    min_idx = i 
     for j in range(i+1, len(A)): 
+        # If any element is smaller than value at minimum index, update minimum index
         if A[min_idx] > A[j]: 
             min_idx = j      
-    # Swap the found minimum element with  
-    # the first element         
+    # Swap the found minimum element with current index         
     A[i], A[min_idx] = A[min_idx], A[i] 
   
 # Driver code to test above 
