c69f11e9decb9b0443ed6c5267b79be3baf2cf03
Mohammed Suhail <Suhail727@users.noreply.github.com>
2019-04-23 13:04:34 +0530

    Updated Bubble sort with comments

--- Sorting (Bubble Sort).py
+++ Sorting (Bubble Sort).py
@@ -3,7 +3,11 @@ A = [64, 25, 12, 22, 11]
   
 # Traverse through all array elements 
 for i in range(len(A)): 
+    # Set flag to check that if in any pass, if none of the elements are swapped, then 
+    # the array is already sorted, hence we can break the outer loop.
     flag=0
+    # len(A)-i-1 is to stop checking the sorted elements 
+    # which are at the end of the list after each pass
     for j in range(len(A)-i-1):
       if(A[j+1]<A[j]):
         A[j+1],A[j] = A[j],A[j+1]
